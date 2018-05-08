b3857c77b33a43795f24376aa2779e643e267174
Ion Mardari <john.mardari@gmail.com>
2018-05-08 18:45:05 -0400

    Challenge complete

--- TextFieldChallenge/DollarDelegate.swift
+++ TextFieldChallenge/DollarDelegate.swift
@@ -11,5 +11,31 @@ import UIKit
 
 class DollarDelegate: NSObject, UITextFieldDelegate {
     
+    var amount = ""
     
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let number = NumberFormatter()
+        number.minimumIntegerDigits = 1
+        number.minimumFractionDigits = 2
+        
+        func formating() {
+            let decimalNumber = NSDecimalNumber(string: amount).multiplying(by: 0.01)
+            let newString = "$" + number.string(from: decimalNumber)!
+            textField.text = newString
+        }
+        
+        if string.count > 0 {
+            amount += string
+            formating()
+        } else {
+            amount = String(amount.dropLast())
+            if amount.count > 0 {
+                formating()
+            } else {
+                textField.text = "$0.00"
+            }
+        }
+        return false
+    }
 }
--- TextFieldChallenge/ZipCodeDelegate.swift
+++ TextFieldChallenge/ZipCodeDelegate.swift
@@ -11,5 +11,13 @@ import UIKit
 
 class ZipCodeDelegate: NSObject, UITextFieldDelegate {
     
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let currentString = textField.text! as NSString
+        let newString: NSString = currentString.replacingCharacters(in: range, with: string) as NSString
+        
+        return newString.length <= 5
+    }
+    
     
 }
